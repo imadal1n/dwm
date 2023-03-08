f6a2326072301410e653c51b23edb9a3b7380748
imadalin <imadalin@gmail.com>
2023-03-08 20:11:31 +0200

    change focus window border color

--- config.def.h
+++ config.def.h
@@ -14,14 +14,14 @@ static const char dmenufont[]       = "monospace:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#e9a178"; /*"#444444"; */
+static const char col_gray2[]       = "#950101"; /*"#444444"; */
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#a84448"; /*"#005577";*/
+static const char col_cyan[]        = "#3E065F"; /*"#005577";*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
